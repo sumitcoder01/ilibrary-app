c029fcfda87ca6ef22aaef8f3de50c672e537267
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-02-10 02:12:43 +0530

    commit: AddBook Page added

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -1,7 +1,166 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { IoMdSend } from "react-icons/io";
+import Logo from "../assets/library-logo.svg";
+import { useAuth } from "../context/authContext";
+
 export default function AddBook() {
+  const { user } = useAuth();
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    price: "",
+    author: "",
+    image: null,
+  });
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.type === "file") {
+      const fileInput = e.target as HTMLInputElement;
+      const file = fileInput.files ? fileInput.files[0] : null;
+      setFormData({ ...formData, [e.target.name]: file });
+    } else {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+  };
+
+  const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ ...formData, sellerId: user ? user.uid : "" });
+    setFormData({
+      title: "",
+      description: "",
+      price: "",
+      author: "",
+      image: null,
+    });
+  };
+
   return (
-    <div>
-      AddBook Works!
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img className="mx-auto h-10 w-10" src={Logo} alt="ILibrary" />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Add book for sell
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleOnSubmit}>
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Title
+            </label>
+            <div className="mt-2">
+              <input
+                id="title"
+                name="title"
+                type="text"
+                placeholder="Enter book title"
+                required
+                minLength={4}
+                value={formData.title}
+                onChange={handleOnChange}
+                className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 px-3 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Description
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="description"
+                name="description"
+                placeholder="Enter book description"
+                required
+                value={formData.description}
+                onChange={handleOnChange}
+                className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 px-3 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+            {formData.description.length !== 0 && formData.description.length < 3 &&
+              <div className="mt-3 mb-1 text-center text-xs text-red-700">Description must be greater then 3</div>
+            }
+          </div>
+          <div>
+            <label
+              htmlFor="price"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Price
+            </label>
+            <div className="mt-2">
+              <input
+                id="price"
+                name="price"
+                type="number"
+                placeholder="Enter book price"
+                required
+                value={formData.price}
+                onChange={handleOnChange}
+                className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 px-3 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="author"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Author
+            </label>
+            <div className="mt-2">
+              <input
+                id="author"
+                name="author"
+                type="text"
+                placeholder="Enter book author name"
+                required
+                minLength={4}
+                value={formData.author}
+                onChange={handleOnChange}
+                className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 px-3 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="image"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Image
+            </label>
+            <div className="mt-2">
+              <input
+                id="image"
+                name="image"
+                type="file"
+                accept="image/*"
+                required
+                onChange={handleOnChange}
+                className="block w-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 px-3 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              disabled={formData.description.length < 3}
+              className="flex w-full items-center justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-600"
+            >
+              <IoMdSend className="mr-2" /> Add Book
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
+
